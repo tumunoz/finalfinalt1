ef7f9ade6509cfdeecf550f381eb6f66aca15481
Tomas Munoz <tumunoz@uc.cl>
2017-03-14 01:52:59 +0000

    Admin Added

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,8 @@
+class AdminController < ApplicationController
+    
+    attr_accessor :articles
+    
+    def getArticles
+    @articles = Article.last(10).reverse
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  
+  get 'admin/getArticles'
+  
   root :to => "articles#index"
 end
